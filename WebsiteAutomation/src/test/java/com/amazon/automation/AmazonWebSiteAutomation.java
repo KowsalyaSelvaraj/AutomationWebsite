b006c63bc0acd851953ca64e97f6a40ffec128ae
lap <lap@DESKTOP-E9ARFLP>
2023-07-05 17:49:44 +0530

    Initial Commit

---
+++ WebsiteAutomation/src/test/java/com/amazon/automation/AmazonWebSiteAutomation.java
@@ -0,0 +1,12 @@
+package com.amazon.automation;
+
+import org.testng.annotations.Test;
+
+public class AmazonWebSiteAutomation {
+
+	@Test
+	public void runTest() {
+		System.out.println("Hello java code");
+		
+	}
+}
